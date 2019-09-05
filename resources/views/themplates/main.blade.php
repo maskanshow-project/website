d1c6b8515fa2d83955e733a6a525768272f73bc6
Amir Khadangi <amirkhadangi920@Gmail.com>
2019-09-06 01:34:40 +0430

    An speed imporvment on website

--- resources/views/themplates/main.blade.php
+++ resources/views/themplates/main.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/img/favicon/site.webmanifest">
+        <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+
         @php
             $path = request()->getPathInfo();
 
@@ -51,16 +57,16 @@
         
         <link rel="stylesheet" href="/css/app.css">
         <!-- Fonts -->
-        <link href="/fonts/flaticon/flaticon.css" rel="stylesheet">
-        <link rel="stylesheet" href="/vendors/linericon/style.css">
-        <link rel="stylesheet" href="/fonts/fontawesome/css/all.css">
+        <link href="/fonts/flaticon/flaticon.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="/vendors/linericon/style.min.css">
+        <link rel="stylesheet" href="/fonts/fontawesome/css/all.min.css">
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="/css/bootstrap.css">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
         <link rel="stylesheet" href="/vendors/owl-carousel/owl.carousel.min.css">
-        <link rel="stylesheet" href="/vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="/vendors/animate-css/animate.min.css">
         <!-- main css -->
-        <link rel="stylesheet" href="/css/style.css">
-        <link rel="stylesheet" href="/css/responsive.css">
+        <link rel="stylesheet" href="/css/style.min.css">
+        <link rel="stylesheet" href="/css/responsive.min.css">
     </head>
     <body>
 
@@ -68,10 +74,11 @@
             <App></App>
         </div>
 
+        <script src="/js/manifest.js"></script>
+        <script src="/js/vendor.js"></script>
         <script src="/js/app.js"></script>
         <script src="/js/jquery-3.2.1.min.js"></script>
         <script src="/js/stellar.js"></script>
-        <script src="/js/bootstrap.min.js"></script>
         <script src="/vendors/owl-carousel/owl.carousel.min.js"></script>
     </body>
 </html>
