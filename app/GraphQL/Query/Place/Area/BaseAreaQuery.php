313269d09c9d7a2fa973bad35a276060b6902d5d
Amir Khadangi <amirkhadangi920@Gmail.com>
2020-03-31 11:47:10 +0430

    fix areas per page bug

--- app/GraphQL/Query/Place/Area/BaseAreaQuery.php
+++ app/GraphQL/Query/Place/Area/BaseAreaQuery.php
@@ -10,4 +10,16 @@ class BaseAreaQuery extends MainQuery
     use AreaProps;
 
     protected $acceptable = false;
+
+    /**
+     * Get the dynamik per_page property of the models
+     *
+     * @param int|10 $max
+     * @param int|100 $min
+     * @return void
+     */
+    public function getPerPage($args, int $max = 100, int $min = 1)
+    {
+        return 30;
+    }
 }
